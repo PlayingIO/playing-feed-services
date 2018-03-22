10397772c1dd54e3e152fe16723f46d5f54a235d
forafish <fora.luo@gmail.com>
2018-03-23 01:15:03 +0800

    refactoring with async/await

--- src/services/activity/activity.service.js
+++ src/services/activity/activity.service.js
@@ -23,29 +23,27 @@ class ActivityService extends Service {
     this.hooks(defaultHooks(this.options));
   }
 
-  create (data, params) {
+  async create (data, params) {
     const svcFeeds = this.app.service('feeds');
     assert(data.feed && data.feed.indexOf(':undefined') === -1, 'data.feed is undefined');
     assert(data.actor && data.actor.indexOf(':undefined') === -1, 'data.actor is undefined');
     assert(data.verb, 'data.verb is not provided');
     assert(data.object && data.object.indexOf(':undefined') === -1, 'data.object is undefined');
-    
-    return super.create(fp.dissoc('cc', data), params)
-      .then((activity) => {
-        if (data.cc && data.cc.length > 0) {
-          return Promise.all(data.cc.map((cc) => {
-            return svcFeeds.get(cc).then((feed) => {
-              if (feed) {
-                data = fp.compose(
-                  fp.assoc('feed', feed.id),
-                  fp.dissoc('cc')
-                )(data);
-                return this.create(data);
-              }
-            });
-          }));
-        }
-      });
+
+    const copyFeed = async (cc) => {
+      const feed = await svcFeeds.get(cc);
+      if (feed) {
+        data = fp.assoc('feed', feed.id,
+               fp.dissoc('cc', data));
+        return this.create(data);
+      }
+    };
+
+    const activity = await super.create(fp.dissoc('cc', data), params);
+    if (data.cc && data.cc.length > 0) {
+      await Promise.all(fp.map(copyFeed, data.cc));
+    }
+    return activity;
   }
 }
 
