14653ca686b2d2d1ae569699f4a9574daa0e4dcb
forafish <fora.luo@gmail.com>
2018-03-28 17:53:48 +0800

    fix feed_follow_many job

--- src/services/feed/feed.jobs.js
+++ src/services/feed/feed.jobs.js
@@ -8,25 +8,23 @@ export default function (app, options) {
   const agenda = app.agenda;
   assert(agenda, 'agenda not configured properly, check your app');
 
-  // follow many user feeds
+  // follow many feeds
   agenda.define('feed_follow_many', async function (job, next) {
     debug('===== RUN feed_follow_many =====', job.attrs.data);
-    const { user, targets, limit } = job.attrs.data;
-    if (user && targets && limit) {
+    const { feed, targets, limit } = job.attrs.data;
+    if (feed && targets && limit && targets.length > 0) {
       const svcFeeds = app.service('feeds');
-      const feed = await svcFeeds.get(`user:${user}`);
-      const targetFeeds = await svcFeeds.get({
-        query: {
-          id: { $in: fp.map(fp.concat('user:'), targets) },
-          $select: 'activities,*'
-        },
+      const svcActivities = app.service('activities');
+      const getActivities = fp.map((target) => svcActivities.find({
+        query: { feed: target, $limit: limit },
         paginate: false
-      });
-      if (feed && targetFeeds.length > 0) {
-        const activities = fp.flatMap(fp.prop('activities'), targetFeeds);
-        await svcFeeds.action('addMany').patch(feed.id, activities);
-        next();
+      }));
+      const results = await Promise.all(getActivities(targets));
+      if (results.length > 0) {
+        const activities = fp.flatten(results);
+        await svcFeeds.action('addMany').patch(feed, activities);
       }
+      next();
     } else {
       console.error('feed_follow_many job is not provided:', job.attrs.data);
     }
