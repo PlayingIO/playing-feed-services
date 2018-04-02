266eeb82b992a3c2b73c8814f8f3eb99124596a0
forafish <fora.luo@gmail.com>
2018-04-03 03:36:34 +0800

    add addActivity action

--- src/services/aggregated-feed/aggregated-feed.service.js
+++ src/services/aggregated-feed/aggregated-feed.service.js
@@ -3,6 +3,7 @@ import makeDebug from 'debug';
 import { Service, createService } from 'mostly-feathers-mongoose';
 import fp from 'mostly-func';
 
+import { addActivities, formatAggregation } from '../../helpers';
 import AggregatedFeedModel from '../../models/aggregated-feed.model';
 import defaultHooks from './aggregated-feed.hooks';
 
@@ -10,7 +11,10 @@ const debug = makeDebug('playing:feed-services:aggregated-feeds');
 
 const defaultOptions = {
   id: 'id',
-  name: 'aggregated-feeds'
+  name: 'aggregated-feeds',
+  mergeMaxLength: 20, // number of aggregated items to search to see if we match
+                      // or create a new aggregated activity
+  trimChance: 0.01    // the chance to trim the feed, not to grow to infinite size
 };
 
 export class AggregatedFeedService extends Service {
@@ -26,6 +30,7 @@ export class AggregatedFeedService extends Service {
 
   async get (id, params) {
     assert(id && id.indexOf(':') > 0, 'invalid feed id');
+    assert(id.startsWith('aggregated'), 'feed id is not an aggregated feed');
     if (params && params.__action) {
       return super._action('get', params.__action, id, null, params);
     }
@@ -35,6 +40,25 @@ export class AggregatedFeedService extends Service {
       throw new Error('aggregated-feed target is undefined');
     }
     return super._upsert(null, { id, group, target });
+  }
+
+  /**
+   * Add an activity
+   */
+  async _addActivity (id, data, params, feed) {
+    assert(feed, 'feed is not exists.');
+    assert(data.actor && data.verb && data.object, 'activity is not provided.');
+    data.feed = feed.id;
+    data.group = formatAggregation(feed.foramt, data);
+
+    const svcActivities = this.app.service('aggregations');
+    await svcActivities.create(data);
+
+    // trim the feed sometimes
+    if (Math.random() <= this.options.trimChance) {
+      await this._trim(id, null, null, feed);
+    }
+    return feed;
   }
 }
 
