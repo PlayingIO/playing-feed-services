20a0191650bc6d5f63c0c6401cf7779ca64aaa15
forafish <fora.luo@gmail.com>
2018-04-04 23:02:35 +0800

    aggregated-feed and notification-feed both use aggregation activity model

--- src/models/aggregation.model.js
+++ src/models/aggregation.model.js
@@ -1,5 +1,5 @@
+import fp from 'mostly-func';
 import activity from './activity.model';
-import { addMany, removeMany } from './helpers';
 
 const options = {
   strict: false
@@ -15,6 +15,87 @@ const fields = {
   group: { type: String, required: true },     // group by aggregation format result
   seenAt: { type: Date },                      // user opened/browsed the activity
   readAt: { type: Date }                       // user engaged with the activity
+};
+
+// add many
+const addMany = (mongoose, model) => (activities, limit = 15) => {
+  if (!Array.isArray(activities)) activities = [activities];
+  const Aggregation = mongoose.model(model);
+
+  const groupedActivities = fp.groupWith(a => a.feed + a.group + a.verb, activities);
+  return new Promise((resolve, reject) => {
+    // bulk with unordered to increase performance
+    const bulk = Aggregation.collection.initializeUnorderedBulkOp();
+    groupedActivities.forEach(items => {
+      // add timestamp fields
+      const { feed, group, verb } = items[0];
+      items = fp.map(fp.pipe(
+        fp.assoc('createdAt', new Date()),
+        fp.assoc('updatedAt', new Date()),
+        fp.dissoc('group')
+      ), items);
+      // bulk upsert
+      bulk.find({
+        feed, group, verb,
+        type: 'aggregation',
+        [`activities.${limit-1}`]: { $exists: false } // max size to insert
+      }).upsert().updateOne({
+        $setOnInsert: { createdAt: new Date() },
+        $currentDate: { updatedAt: true },
+        $push: {
+          activities: {
+            $each: items,
+            $sort: { updatedAt: -1 }
+          }
+        }
+      });
+    });
+    bulk.execute((err, result) => {
+      if (err) return reject(err);
+      return resolve(result.toJSON());
+    });
+  });
+};
+
+// remove many
+const removeMany = (mongoose, model) => (activities) => {
+  if (!Array.isArray(activities)) activities = [activities];
+  const Aggregation = mongoose.model(model);
+
+  return new Promise((resolve, reject) => {
+    // bulk with unordered to increase performance
+    const bulk = Aggregation.collection.initializeUnorderedBulkOp();
+    activities.forEach(activity => {
+      // add timestamp fields
+      const { id, foreignId } = activity;
+      if (id) {
+        bulk.find({
+          'activities._id': id
+        }).updateOne({
+          $pull: {
+            activities: { _id: id }
+          }
+        });
+      } else {
+        bulk.find({
+          'activities.foreignId': foreignId
+        }).updateOne({
+          $pull: {
+            activities: { foreignId }
+          }
+        });
+      }
+    });
+    bulk.execute((err, result) => {
+      if (err) return reject(err);
+      // remove aggregation activity with empty activities
+      Aggregation.collection.remove({
+        type: 'aggregation',
+        activities: { $size: 0 }
+      });
+      return resolve(result.toJSON());
+    });
+  });
 };
 
 export default function model (app, name) {
