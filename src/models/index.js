10ceac899dac1aee535fa0148e35a7d1fc837c07
forafish <fora.luo@gmail.com>
2018-04-22 04:46:16 +0800

    Fix export entities/models name

--- src/models/index.js
+++ src/models/index.js
@@ -5,5 +5,5 @@ import path from 'path';
 const modelFiles = glob.sync(path.join(__dirname, './*.model.js'));
 export default Object.assign({}, ...modelFiles.map(file => {
   const name = path.basename(file, '.model.js');
-  return { name: require(file).default };
+  return { [name]: require(file).default };
 }));
