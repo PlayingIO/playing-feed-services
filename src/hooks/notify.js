468091fd647423425084cb2972d26f3238a15bf7
forafish <fora.luo@gmail.com>
2018-05-04 17:58:09 +0800

    Fix notify hook

--- src/hooks/notify.js
+++ src/hooks/notify.js
@@ -4,10 +4,13 @@ import fp from 'mostly-func';
 import { addActivity } from '../helpers';
 
 export default function notify (event, notifies) {
-  return context => {
+  return async context => {
     if (notifies[event]) {
       const [activity, ...feeds] = notifies[event](context);
-      return addActivity(context.app, activity, feeds);
+      if (activity && fp.isNotEmpty(feeds)) {
+        await addActivity(context.app, activity, feeds);
+        return context;
+      }
     } else {
       throw new Error('No such notifer found for ' + event);
     }
