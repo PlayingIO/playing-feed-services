8b96f28b7dadd02e3a554090539c2498e7f1746e
forafish <fora.luo@gmail.com>
2018-05-17 17:53:27 +0800

    Fix typo

--- src/hooks/notify.js
+++ src/hooks/notify.js
@@ -3,10 +3,10 @@ import fp from 'mostly-func';
 
 import { addActivity } from '../helpers';
 
-export default function notify (event, notifies) {
+export default function notify (event, notifiers) {
   return async context => {
-    if (notifies[event]) {
-      const [activity, ...feeds] = notifies[event](context);
+    if (notifiers[event]) {
+      const [activity, ...feeds] = notifiers[event](context);
       if (activity && fp.isNotEmpty(feeds)) {
         await addActivity(context.app, activity, feeds);
         return context;
