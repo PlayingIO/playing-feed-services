9ff7d570417d9ab0a836f16f1d0c7c7a96d50eb2
forafish <fora.luo@gmail.com>
2018-05-18 01:50:05 +0800

    Fix addActivity with cc

--- src/helpers.js
+++ src/helpers.js
@@ -94,9 +94,11 @@ export const validateUpdateActivity = (item) => {
  */
 export const addActivity = async (app, activity, ...feeds) => {
   const svcFeedsActivities = app.service('feeds/activities');
-
   feeds = fp.uniq(fp.flatten(feeds));
-  const first = fp.head(feeds), tail = fp.tail(feeds);
+
+  // aggregated feed do not support cc, first feed should be a flat feed
+  const first = fp.find(fp.eqBy(getFeedType, 'flat'), feeds);
+  const tail = fp.without([first], feeds);
 
   // carbon copy to tail feeds
   activity.cc = (activity.cc || []).concat(tail);
